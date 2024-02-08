8dfb5a7fb71b3d3ccb51bd5187bdb30eeff6abf2
Frank <frank.snelling03@gmail.com>
2024-02-08 14:27:43 +0000

    Main window set up.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-code
+import tkinter as tk
+from tkinter import ttk
+
+class App(tk.Tk):
+    """This class encapsulates the main window"""
+    def __init__(self):
+        # main setup
+        super().__init__()
+        self.title("London Darwin Time Converter") # name window
+        self.geometry('1000x400') # change window size
+
+        # widgets
+
+        # run
+        self.mainloop()
+
